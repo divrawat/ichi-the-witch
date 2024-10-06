228d274daed70e68be116b57b86480d9f6c6eaf2
divrawat <divrawat2001@gmail>
2024-10-06 14:54:42 +0530

    sef0ers5g

---
+++ config.js
@@ -0,0 +1,129 @@
+// export const DOMAIN = 'http://localhost:3000';
+export const DOMAIN = 'https://www.ichithewitch.online';
+
+export const DOMAIN_NAME = 'ichithewitch.online';
+
+export const IMAGES_SUBDOMAIN = 'https://images.ichithewitch.online/ichi-the-witch';
+export const IMAGES_SUBDOMAIN_LOGO = 'https://images.mangachimp.com';
+
+// export const MANGA_DESCRIPTION0 = `<a href="${DOMAIN}"><u><b>Vinland Saga</b></u></a>`;
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u><b>Ichi the Witch</b></u></a> is a fantasy manga set in a world where magic exists in the form of living creatures called Majiks. Witches, specialized hunters. The story revolves around Ichi, a young boy living in the mountains who hunts for a living. After defeating a Majik, he unexpectedly becomes the first male witch. This discovery challenges the long-held belief and sets Ichi on a journey to uncover the secrets of magic and his own abilities.`;
+
+
+
+export const MANGA_NAME = 'Ichi the Witch';
+export const MANGA_AUTHOR = 'Osamu Nishi';
+export const MANGA_RELEASE = '2024';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Fujino Ōmori';
+export const MANGA_STUDIO = 'None';
+export const MANGA_GENRE = 'Action, Adventure, Drama, Shonen, Fantasy, School';
+export const MANGA_TYPE = 'Manhwa';
+export const HEADER_MANGA_DESC = "Ichi the Witch is a fantasy manga set in a world where magic exists in the form of living creatures called Majiks. Witches, specialized hunters. The story revolves around Ichi, a young boy living in the mountains who hunts for a living. After defeating a Majik, he unexpectedly becomes the first male witch. This discovery challenges the long-held belief and sets Ichi on a journey to uncover the secrets of magic and his own abilities."
+
+export const CHAPTER_PREFIX = 'chapter';
+export const IMAGE_PREFIX = 'images/vinland-saga';
+export const NEXT_PREVIOUS_PREFIX = 'chapter';
+export const URL_PREFIX = 'chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/jujutsu-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "";
+
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] w-[75px] rounded-[55%] mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'Ichi The Witch';
+export const NavbarName = "Ichi The Witch";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN_NAME}. ${HEADER_MANGA_DESC}`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "The manga Ichi the Witch is a dark fantasy series that follows the life of Ichi, a young witch with a troubled past. Ichi's childhood was marked by tragedy, as she witnessed the destruction of her village and the murder of her family by a mysterious force. Haunted by these events, Ichi sets out on a journey to uncover the truth behind her past and seek revenge on those responsible."
+    },
+    {
+        id: 2,
+        content: "Along the way, Ichi encounters a variety of characters, including a talking crow named Kuro, a mysterious swordsman named Kage, and a young woman named Yumi who shares a similar tragic past. As Ichi delves deeper into the world of witches and magic, she discovers a hidden society that is struggling to survive in a world hostile to their kind."
+    },
+    {
+        id: 3,
+        content: "The manga explores themes of revenge, loss, and the nature of good and evil. Ichi is a complex character who grapples with her own dark impulses while trying to maintain her humanity. The story is filled with action, suspense, and emotional depth, making it a compelling read for fans of dark fantasy."
+    }
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Chapter 1', href: `${DOMAIN}/chapter-1` },
+    { text: 'Chapter 2', href: `${DOMAIN}/chapter-2` },
+    { text: 'Chapter 3', href: `${DOMAIN}/chapter-3` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+
+
+
+
+
+export const chaptersData =
+
+    [
+        {
+            "chapterNumber": "1",
+            "numImages": "50",
+        },
+        {
+            "chapterNumber": "2",
+            "numImages": "24",
+        },
+        {
+            "chapterNumber": "3",
+            "numImages": "22",
+        },
+        {
+            "chapterNumber": "4",
+            "numImages": "18",
+        }
+    ]
+
+
+
+
+
+export const last5chapters = [
+
+    {
+        "chapterNumber": "1",
+        "numImages": "50",
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": "24",
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": "22",
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": "18",
+    }
+]
